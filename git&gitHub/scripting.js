60fd3925ebc50c93b212d42d31ddaeecd20f14e5
ROHIT NITTAWADEKAR <rohitnittawadekar07@gmail.com>
2023-02-09 15:15:24 +0530

    SETUP Command are ready and complete

---
+++ git&gitHub/scripting.js
@@ -0,0 +1,68 @@
+// console.log("hello user  welcome to the JQury")
+
+
+$(document).ready(function () {
+    $("#setup").click(function () {
+        console.log("hlo function");
+        $("#display-setup-1").slideToggle();
+        $("#display-setup-2").slideToggle();
+    });
+
+    $("#btn-set").click(function () {
+        $("#set-list").show();
+        $("#check-list").hide();
+    });
+
+    $("#btn-check").click(function () {
+        $("#set-list").hide();
+        $("#check-list").show();
+    });
+
+    
+
+    $("#set-name").click(function () {
+        $("#set-name-value").show();
+        $("#set-email-value").hide();
+        $("#set-ui-color-value").hide();
+        $("#check-name-content").hide();
+        $("#check-email-content").hide();
+
+
+    });
+
+    $("#set-email").click(function () {
+        $("#set-email-value").show();
+        $("#set-ui-color-value").hide();
+        $("#set-name-value").hide();
+        $("#check-name-content").hide();
+        $("#check-email-content").hide();
+    });
+
+    $("#set-ui-color").click(function () {
+        $("#set-ui-color-value").show();
+        $("#set-email-value").hide();
+        $("#set-name-value").hide();
+        $("#check-name-content").hide();
+        $("#check-email-content").hide();
+    });
+
+
+    $("#check-name").click(function () {
+        $("#check-name-content").show();
+        $("#check-email-content").hide();
+        $("#set-ui-color-value").hide();
+        $("#set-email-value").hide();
+        $("#set-name-value").hide();
+    });
+
+    $("#check-email").click(function () {
+        $("#check-name-content").hide();
+        $("#check-email-content").show();
+        $("#set-ui-color-value").hide();
+        $("#set-email-value").hide();
+        $("#set-name-value").hide();
+    });
+});
+
+
+
